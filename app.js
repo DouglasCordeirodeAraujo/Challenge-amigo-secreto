6bc114544f42fe43c283cf26abb52e9be161a9dc
Douglas Cordeiro de Araujo <douglas.cordeiro789@gmail.com>
2025-03-14 14:22:11 -0300

    correção de texto, adição de novos verificadores a função SortearAmigo

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
 let amigos = [];
+let sorteados = new Set();
 
 function adicionarAmigo() {
     let nome = document.querySelector('input').value;
@@ -33,11 +34,20 @@ function sortearAmigo() {
     if (amigos.length === 0) {
         alert('Por favor, adicione amigos antes de sortear.');
         return;
-    } else {
-        let sorteiro = Math.floor(Math.random() * amigos.length);
-        let amigoSorteado = amigos[sorteiro];
-
-        document.getElementById('listaAmigos').innerHTML = '';
-        document.getElementById('resultado').innerHTML = `<li>O amigo secreto sorteado é: ${amigoSorteado}</li>`;
     }
+
+    if (sorteados.size === amigos.length) {
+        alert('Todos os amigos já foram sorteados.');
+        return;
+    }
+
+    let amigoSorteado;
+    do {
+        amigoSorteado = amigos[Math.floor(Math.random() * amigos.length)];
+    } while (sorteados.has(amigoSorteado));
+
+    sorteados.add(amigoSorteado);
+
+    document.getElementById('listaAmigos').innerHTML = '';
+    document.getElementById('resultado').innerHTML = `<li>O amigo secreto sorteado é: ${amigoSorteado}</li>`;
 }
