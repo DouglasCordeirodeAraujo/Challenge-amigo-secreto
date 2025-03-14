d02b7bed4dd5576d9517341181bcfafbda9fb796
Douglas Cordeiro de Araujo <douglas.cordeiro789@gmail.com>
2025-03-13 23:14:24 -0300

    no arquivo app.js criei funcionalidade para adicionar amigos a lista e atualizar a exibição.

--- app.js
+++ app.js
@@ -1,1 +1,30 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = [];
+
+function adicionarAmigo() {
+    let nome = document.querySelector('input').value;
+
+    if (nome === '') {
+        alert('Por favor, insira um nome.');
+        return;
+    } else {
+        amigos.push(nome);
+    }
+    limparCampo();
+    AtualizarListaAmigos();
+}
+
+function limparCampo() {
+    document.querySelector('input').value = '';
+}
+
+function AtualizarListaAmigos() {
+    let lista = document.getElementById('listaAmigos');
+    lista.innerHTML = '';
+
+    for (let i = 0; i < amigos.length; i++) {
+        let item = document.createElement('li');
+        item.textContent = amigos[i];
+        lista.appendChild(item);
+    }
+}
